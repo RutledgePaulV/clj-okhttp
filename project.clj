6b59e7d38f2556c4d62ec02d1a17c13cb5514d74
Vincent Pizzo <vincent.pizzo@singlewire.com>
2022-02-03 17:07:57 -0800

    Bump dependencies and resolve jsonista reflection issues for #2

--- project.clj
+++ project.clj
@@ -27,16 +27,16 @@
   :dependencies
   [[org.clojure/clojure "1.10.3"]
    [metosin/muuntaja "0.6.8"]
-   [metosin/jsonista "0.3.1"]
-   [com.squareup.okhttp3/okhttp "4.9.1"]]
+   [metosin/jsonista "0.3.5"]
+   [com.squareup.okhttp3/okhttp "4.9.3"]]
 
   :profiles
   {:test {:resource-paths ["testfiles"]
-          :dependencies   [[clj-http "3.12.1"]
-                           [cheshire "5.10.0"]
+          :dependencies   [[clj-http "3.12.3"]
+                           [cheshire "5.10.2"]
                            [criterium "0.4.6"]
-                           [org.slf4j/slf4j-simple "1.7.30"]
-                           [org.testcontainers/testcontainers "1.15.2"]]}}
+                           [org.slf4j/slf4j-simple "1.7.35"]
+                           [org.testcontainers/testcontainers "1.16.3"]]}}
 
   :cloverage
   {:selector [:coverage]}
