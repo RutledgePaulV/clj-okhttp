4d4b2ee625cc4f577449c194db9e3edb76042f05
Paul Rutledge <rutledgepaulv@gmail.com>
2022-05-09 15:16:46 -0500

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-okhttp/clj-okhttp "0.1.1-SNAPSHOT"
+(defproject clj-okhttp/clj-okhttp "0.1.1"
 
   :description
   "A fast and lightweight clojure http client constructed on top of OkHttp."
